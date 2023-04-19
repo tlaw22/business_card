f66befdabc15e072566eeba58265ea940232ce2e
Tim <timothy.lawless@gmail.com>
2023-04-19 15:58:28 -0400

    Added more layout1 Added Cards

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
-            children: const <Widget>[
+            children: <Widget>[
               CircleAvatar(
                 radius: 75.0,
                 backgroundImage: AssetImage('res/tim.png'),
@@ -28,7 +28,6 @@ class MyApp extends StatelessWidget {
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
                   )),
-
               Text('Java & Flutter Developer',
                   style: TextStyle(
                     fontSize: 18,
@@ -38,11 +37,80 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
 
                   )),
+            const Card(
+              color: Colors.blueGrey,
+              margin: EdgeInsets.symmetric(vertical: 10.0,
+              horizontal: 10),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone,
+                    color: Colors.blue,
+                  ),
+                  title: Text('727-697-7225',
+                  style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Pacifico',
+                  fontSize: 20.0,
 
+                )
+              ),
+            ),
+              ),
+            ),
+              SizedBox(
+                child: Divider(
+                  color: Colors.orange,
+                  height: 20,
+                ),
+              ),
+              const Card(
+                color: Colors.blueGrey,
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                    horizontal: 10),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email,
+                      color: Colors.blue,
+                    ),
+                    title: Text('Jagged-wraith@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Pacifico',
+                          fontSize: 20.0,
+
+                        )
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
+
         ),
+
       ),
     );
   }
 }
+
+/*
+ Row(
+                  children: <Widget>[
+                    Icon(Icons.phone,
+                    color: Colors.blueAccent,
+                    size: 25.0,),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('727-697-7225',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Pacifico',
+                      fontSize: 20.0
+                    ),)
+                  ],
+                ),
+
+ */
